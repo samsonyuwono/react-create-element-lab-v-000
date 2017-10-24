9dbd97b788cebfbe7da46cbae5d41e15c040321a
github username <github email address>
2017-10-24 14:08:43 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,16 +4,15 @@ import ReactDOM from 'react-dom';
 // IMPORTANT!!!! uncomment the lines below to make your tests work
 
 const meInReact =
-  React.createElement('div', {},
+  React.createElement('div', {className: 'me'},
     React.createElement('h1', {}, 'An Awesome Person'),
-      React.createElement('ul', {},
-      [
-        React.createElement('li', {className: 'JavaScript'}, 'JavaScript'),
-        React.createElement('li', {className: 'React'}, 'React'),
-        React.createElement('li', {className: 'Movies'}, 'Movies'),
-        React.createElement('li', {className: 'Ice cream'}, 'Ice Cream'),
-      ]
-    )
+      React.createElement('p', {}, 'Who is learning React'),
+        React.createElement('ul', {className: 'my-interests'}, [
+        React.createElement('li', {}, 'JavaScript'),
+        React.createElement('li', {}, 'React'),
+        React.createElement('li', {}, 'Movies'),
+        React.createElement('li', {}, 'Ice cream'),
+      ])
   );
 
 ReactDOM.render(
